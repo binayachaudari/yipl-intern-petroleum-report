df487ae366834d2aa242c4cbeb31ab675a28b72b
Binaya Chaudhary <binayachaudari@live.com>
2020-08-29 10:08:31 +0545

    Refactored

--- report.js
+++ report.js
@@ -4,13 +4,25 @@ let listOfPetroleumProducts;
 let maxYear;
 let minYear;
 
-const calculateMax = arrayOfData => Math.max(...arrayOfData);
+/**
+ * Get data from the server
+ */
+const getData = async () => {
+  try {
+    const res = await Axios.get(
+      'https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json'
+    );
 
-const calculateMin = arrayOfData => Math.min(...arrayOfData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const calculateAverage = arrayOfData =>
-  (arrayOfData.reduce((acc, cur) => acc + cur) / arrayOfData.length).toFixed(2);
-
+/**
+ * Calculates inverval of years .i.e. length
+ * @param {Number} length Inverval of years
+ */
 const calculateInterval = length => {
   const intervals = [];
   let isCalculated = false;
@@ -27,55 +39,96 @@ const calculateInterval = length => {
   return intervals;
 };
 
-const calculateReport = (data, years) => {
+/**
+ * Calculates Maximum value
+ * @param {Array} arrayOfData Array of Data
+ */
+const calculateMax = arrayOfData =>
+  Math.max(...arrayOfData) === -Infinity ? 0 : Math.max(...arrayOfData);
+
+/**
+ * Caclulates Minimum Value
+ * @param {Array} arrayOfData Array of Data
+ */
+const calculateMin = arrayOfData =>
+  Math.min(...arrayOfData) === Infinity ? 0 : Math.min(...arrayOfData);
+
+/**
+ * Calculates Average value from given array
+ * @param {Array} arrayOfData Array of Data
+ */
+const calculateAverage = arrayOfData => {
+  try {
+    return (
+      arrayOfData.reduce((acc, cur) => acc + cur) /
+      arrayOfData.length.toFixed(2)
+    );
+  } catch (error) {
+    return 0;
+  }
+};
+
+/**
+ * Calculates Report
+ * @param {Array} data Array of Data from server
+ * @param {Array} intervalYears Array of Interval Years
+ */
+const calculateReport = (data, intervalYears) => {
   const report = [];
 
   listOfPetroleumProducts.forEach(item => {
     const filteredByProduct = data.filter(
       product => product.petroleum_product === item
     );
-    for (let currIndex = years.length - 1; currIndex > 0; currIndex -= 2) {
+    for (
+      let currIndex = intervalYears.length - 1;
+      currIndex > 0;
+      currIndex -= 2
+    ) {
       const filteredByDate = filteredByProduct.filter(
         product =>
-          product.year >= years[currIndex - 1] &&
-          product.year <= years[currIndex]
+          product.year >= intervalYears[currIndex - 1] &&
+          product.year <= intervalYears[currIndex]
       );
+
+      const omitedZeroValue = filteredByDate.filter(
+        product => product.sale !== 0
+      );
+
       const payload = {
         Product: item,
-        Year: `${years[currIndex - 1]}-${years[currIndex]}`,
-        Min: calculateMin(filteredByDate.map(product => product.sale)),
-        Max: calculateMax(filteredByDate.map(product => product.sale)),
-        Avg: calculateAverage(filteredByDate.map(product => product.sale)),
+        Year: `${intervalYears[currIndex - 1]}-${intervalYears[currIndex]}`,
+        Min: calculateMin(omitedZeroValue.map(product => product.sale)),
+        Max: calculateMax(omitedZeroValue.map(product => product.sale)),
+        Avg: calculateAverage(omitedZeroValue.map(product => product.sale)),
       };
 
       report.push(payload);
     }
   });
 
+  return report;
+};
+
+/**
+ * Generates Report
+ */
+const generateReport = async () => {
+  const dataFromServer = await getData();
+
+  const listOfYears = [...new Set(dataFromServer.map(product => product.year))];
+  maxYear = Math.max(...listOfYears);
+  minYear = Math.min(...listOfYears);
+
+  listOfPetroleumProducts = [
+    ...new Set(dataFromServer.map(product => product.petroleum_product)),
+  ];
+
+  const intervalOfYears = calculateInterval(5); // Pass years interval as an argument
+
+  const report = calculateReport(dataFromServer, intervalOfYears);
+
   console.log(report);
 };
 
-const getData = async () => {
-  try {
-    const res = await Axios.get(
-      'https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json'
-    );
-
-    const listOfYears = [...new Set(res.data.map(product => product.year))];
-    maxYear = Math.max(...listOfYears);
-    minYear = Math.min(...listOfYears);
-    listOfPetroleumProducts = [
-      ...new Set(res.data.map(product => product.petroleum_product)),
-    ];
-
-    const intervalOfYears = calculateInterval(5);
-
-    calculateReport(res.data, intervalOfYears);
-
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getData();
+generateReport(); // Main
